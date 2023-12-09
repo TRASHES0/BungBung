c46004096037da51cd52e201a179aee53a22d547
bacsal <50982614+bacsal@users.noreply.github.com>
2023-12-09 22:51:42 +0900

    dd
    AI BP_Enemy Create

---
+++ Source/BungBung/EnemyAI_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BungBungCharacter.h"
+#include "EnemyAI_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUNGBUNG_API AEnemyAI_Base : public ABungBungCharacter
+{
+	GENERATED_BODY()
+	
+};
